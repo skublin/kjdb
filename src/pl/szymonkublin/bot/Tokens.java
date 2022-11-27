fe10a1a8b5133c90b7775709af53e96946bb84a4
skublin <szymonkublin@gmail.com>
2022-11-27 13:44:20 +0100

    Update Tokens.java

--- src/pl/szymonkublin/bot/Tokens.java
+++ src/pl/szymonkublin/bot/Tokens.java
@@ -2,6 +2,6 @@ package pl.szymonkublin.bot;
 
 public class Tokens {
 
-    public static final String MIRROR_BOT_TOKEN = "MTA0MTMwODE0MTYxNjM2NTU4OQ.GVOkqC.Pk5SgQwQ8KCTnO6eTn2ccMIe9L_TGPCHcbDNC0";
-    public static final String PLUS_ONE_BOT_TOKEN = "MTA0MTMxMDk5OTAwNjk0MTI0NA.G85oHN.VlpAYF18-ZjV_6whERcWfwWgRQWpPkbVsnpnCk";
+    public static final String MIRROR_BOT_TOKEN = "";
+    public static final String PLUS_ONE_BOT_TOKEN = "";
 }
